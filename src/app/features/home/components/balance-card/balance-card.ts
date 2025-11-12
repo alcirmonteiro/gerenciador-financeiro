782a6494743867ca73cdf1bf8677661fd8bd3269
Alcir Monteiro <alcirmonteiro@gmail.com>
2025-11-12 14:46:47 -0300

    feat: estilização do BalanceCard

--- src/app/features/home/components/balance-card/balance-card.ts
+++ src/app/features/home/components/balance-card/balance-card.ts
@@ -1,5 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+
+type CardType = 'income' | 'outcome' | 'balance';
+type ValueCssClass = 'income' | 'outcome';
 
 @Component({
   selector: 'app-balance-card',
@@ -8,8 +11,19 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './balance-card.scss',
 })
 export class BalanceCard {
-  type = input.required<'income' | 'outcome' | 'balance'>();
+  type = input.required<CardType>();
   label = input.required<string>();
   value = input.required<number>();
 
+  cssClass = computed<ValueCssClass>(() => {
+    if (this.type() === 'income') {
+      return 'income';
+    }
+
+    if (this.type() === 'outcome') {
+      return 'outcome';
+    }
+
+    return this.value() >= 0 ? 'income' : 'outcome'; 
+  });
 }
