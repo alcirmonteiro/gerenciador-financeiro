e6f4e6d78bcc78a9542a6b2e424127b935cf7f41
Alcir Monteiro <alcirmonteiro@gmail.com>
2025-11-12 16:26:52 -0300

    feat: Cria calculo do BalanceCard

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
 
 @Component({
@@ -8,5 +8,12 @@ import { Balance } from './components/balance/balance';
   styleUrl: './home.scss',
 })
 export class Home {
+
+  transactions = signal([
+    { value: 1000, type: 'income' },
+    { value: 500, type: 'income' },
+    { value: 1000, type: 'outcome' },
+    { value: 500, type: 'outcome' },
+  ]);
 
 }
