f5979298ae2b9a9b09530729e4f3e048814c1bdc
Alcir Monteiro <alcirmonteiro@gmail.com>
2025-11-14 11:14:49 -0300

    feat: ajusta layout do NoTransaction

--- src/app/features/home/components/no-transactions/no-transactions.ts
+++ src/app/features/home/components/no-transactions/no-transactions.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-no-transactions',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './no-transactions.html',
   styleUrl: './no-transactions.scss',
 })
