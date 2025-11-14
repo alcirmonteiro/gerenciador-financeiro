9e4bdf912ab1af04205204e06fd375ef03e7b0df
ALCIR DOS SANTOS MONTEIRO <admonteiro@icatuseguros.com.br>
2025-11-14 10:42:07 -0300

    feat(NoTransactions): Criação do ccomponente

---
+++ src/app/features/home/components/no-transactions/no-transactions.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-transactions',
+  imports: [],
+  templateUrl: './no-transactions.html',
+  styleUrl: './no-transactions.scss',
+})
+export class NoTransactions {
+
+}
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,20 +3,16 @@ import { Balance } from './components/balance/balance';
 import { TransactionItem } from "./components/transaction-item/transaction-item";
 import { transaction } from '../../shared/transaction/interfaces/transaction';
 import { transactionType } from '../../shared/transaction/enums/transaction.enum';
+import { NoTransactions } from './components/no-transactions/no-transactions';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransactions],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
 
-  transactions = signal<transaction[]>([
-    { title: 'Salário', value: 1000, type: transactionType.INCOME },
-    { title: 'Aluguel', value: 500, type: transactionType.INCOME  },
-    { title: 'Curso', value: 1000, type: transactionType.OUTCOME  },
-    { title: 'faculdade', value: 500, type: transactionType.OUTCOME },
-  ]);
+  transactions = signal<transaction[]>([ ]);
 
 }
